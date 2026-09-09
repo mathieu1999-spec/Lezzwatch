aa00f9113b5b097ac345bebd097a9cd5d9df4611
sosophia <s.ilustre@gmail.com>
2026-09-09 18:09:19 +0800

    added import

--- app/src/main/java/com/lezzwatch/app/ui/navigation/LezzwatchNavHost.kt
+++ app/src/main/java/com/lezzwatch/app/ui/navigation/LezzwatchNavHost.kt
@@ -1,5 +1,7 @@
 package com.lezzwatch.app.ui.navigation
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Coffee
